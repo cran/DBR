9dbf9043b3d1a36922803b4aac2e8810457b1df7
Alireza Mahani <alireza.s.mahani@gmail.com>
2022-03-24 07:40:21 +0000

    version 1.2.3

--- inst/doc/DBR.R
+++ inst/doc/DBR.R
@@ -7,7 +7,7 @@ old <- options(prompt = "R> ", continue = "+  ", width = 70, useFancyQuotes = FA
 
 
 ###################################################
-### code chunk number 2: DBR.Rnw:275-279
+### code chunk number 2: DBR.Rnw:271-275
 ###################################################
 library("DBR")
 data("pain")
@@ -16,7 +16,7 @@ df$age <- as.integer(df$age)
 
 
 ###################################################
-### code chunk number 3: DBR.Rnw:281-285 (eval = FALSE)
+### code chunk number 3: DBR.Rnw:277-281 (eval = FALSE)
 ###################################################
 ## source("../R/util.R")
 ## source("../R/dbr.R")
@@ -25,19 +25,19 @@ df$age <- as.integer(df$age)
 
 
 ###################################################
-### code chunk number 4: DBR.Rnw:288-289
+### code chunk number 4: DBR.Rnw:284-285
 ###################################################
 summary(df)
 
 
 ###################################################
-### code chunk number 5: DBR.Rnw:294-295
+### code chunk number 5: DBR.Rnw:290-291
 ###################################################
 plot(df)
 
 
 ###################################################
-### code chunk number 6: DBR.Rnw:300-304 (eval = FALSE)
+### code chunk number 6: DBR.Rnw:296-300 (eval = FALSE)
 ###################################################
 ## ret <- with(df, {
 ##   print(cor.test(severity, interference, method = "spearman"))
@@ -46,7 +46,7 @@ plot(df)
 
 
 ###################################################
-### code chunk number 7: DBR.Rnw:306-314
+### code chunk number 7: DBR.Rnw:302-310
 ###################################################
 ret <- with(df, {
   suppressWarnings(
@@ -59,7 +59,7 @@ ret <- with(df, {
 
 
 ###################################################
-### code chunk number 8: DBR.Rnw:320-324 (eval = FALSE)
+### code chunk number 8: DBR.Rnw:316-320 (eval = FALSE)
 ###################################################
 ## est.1 <- dbr(
 ##   formula = interference ~ severity + age
@@ -68,31 +68,31 @@ ret <- with(df, {
 
 
 ###################################################
-### code chunk number 9: DBR.Rnw:326-327
+### code chunk number 9: DBR.Rnw:322-323
 ###################################################
 est.1 <- readRDS("est_1.rds")
 
 
 ###################################################
-### code chunk number 10: DBR.Rnw:332-333 (eval = FALSE)
+### code chunk number 10: DBR.Rnw:328-329 (eval = FALSE)
 ###################################################
 ## summary(est.1)
 
 
 ###################################################
-### code chunk number 11: DBR.Rnw:340-341
+### code chunk number 11: DBR.Rnw:336-337
 ###################################################
 cat(readLines('summary_1.txt'), sep = '\n')
 
 
 ###################################################
-### code chunk number 12: DBR.Rnw:352-353
+### code chunk number 12: DBR.Rnw:348-349
 ###################################################
 setdiff(0:70, round(7 * sort(unique(df$interference))))
 
 
 ###################################################
-### code chunk number 13: DBR.Rnw:361-370 (eval = FALSE)
+### code chunk number 13: DBR.Rnw:357-366 (eval = FALSE)
 ###################################################
 ## est.2 <- dbr(
 ##   formula = interference ~ severity + age
@@ -106,19 +106,19 @@ setdiff(0:70, round(7 * sort(unique(df$interference))))
 
 
 ###################################################
-### code chunk number 14: DBR.Rnw:373-374 (eval = FALSE)
+### code chunk number 14: DBR.Rnw:369-370 (eval = FALSE)
 ###################################################
 ## summary(est.2)
 
 
 ###################################################
-### code chunk number 15: DBR.Rnw:381-382
+### code chunk number 15: DBR.Rnw:377-378
 ###################################################
 cat(readLines('summary_2.txt'), sep = '\n')
 
 
 ###################################################
-### code chunk number 16: DBR.Rnw:392-397 (eval = FALSE)
+### code chunk number 16: DBR.Rnw:388-393 (eval = FALSE)
 ###################################################
 ## pred_point <- predict(est.2, newdata = df, type = "point")
 ## hist(pred_point, breaks = 100, col = "grey"
@@ -128,7 +128,7 @@ cat(readLines('summary_2.txt'), sep = '\n')
 
 
 ###################################################
-### code chunk number 17: DBR.Rnw:400-405
+### code chunk number 17: DBR.Rnw:396-401
 ###################################################
 pred_point <- readRDS("pred_point.rds")
 hist(pred_point, breaks = 100, col = "grey"
@@ -138,7 +138,7 @@ hist(pred_point, breaks = 100, col = "grey"
 
 
 ###################################################
-### code chunk number 18: DBR.Rnw:410-415 (eval = FALSE)
+### code chunk number 18: DBR.Rnw:406-411 (eval = FALSE)
 ###################################################
 ## pred_sample <- predict(est.2, newdata = df, type = "sample")
 ## hist(pred_sample, breaks = 100, col = "grey"
@@ -148,7 +148,7 @@ hist(pred_point, breaks = 100, col = "grey"
 
 
 ###################################################
-### code chunk number 19: DBR.Rnw:418-423
+### code chunk number 19: DBR.Rnw:414-419
 ###################################################
 pred_sample <- readRDS("pred_sample.rds")
 hist(pred_sample, breaks = 100, col = "grey"
@@ -158,13 +158,13 @@ hist(pred_sample, breaks = 100, col = "grey"
 
 
 ###################################################
-### code chunk number 20: DBR.Rnw:443-444
+### code chunk number 20: DBR.Rnw:439-440
 ###################################################
 sessionInfo()
 
 
 ###################################################
-### code chunk number 21: DBR.Rnw:447-448
+### code chunk number 21: DBR.Rnw:443-444
 ###################################################
 options(old)
 
